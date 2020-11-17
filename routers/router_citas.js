dd83364d734cfd5f8d7589d03295da31310ea410
santiagorodriguez-manzaneque <santiagormj@icloud.com>
2020-11-17 17:40:54 +0100

    1

--- routers/router_citas.js
+++ routers/router_citas.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const service_citas = require('../services/services_citas');
 
-router.get('/c', service_citas.listarCitas);
+router.get('/', service_citas.listarCitas);
 router.get('/pending', service_citas.citasPendientes );
 router.delete('/', service_citas.cancelarCita);
 router.post('/', service_citas.crearCita);
 
 
-module.exports.routes = router;
+module.exports = router;
